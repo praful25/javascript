9dbabd0726551b3b3e5d41cad61df005cc32ff7e
Praful Bagde <78199648+praful25@users.noreply.github.com>
2024-06-12 18:58:16 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+// number => 25
+// string => "Praful"
+// boolean => true/false
+// null => stand alone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
