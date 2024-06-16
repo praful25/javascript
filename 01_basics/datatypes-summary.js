a2b1c1a2a53e83a0f4919ba57c583b5dee60902d
Praful Bagde <78199648+praful25@users.noreply.github.com>
2024-06-16 09:48:31 +0000

    datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -19,4 +19,25 @@ const myDetails = {
 const myFunc = function myFuntion() {
     console.log("Hello World");
 }
-console.log(typeof myFunc);
+// console.log(typeof myFunc);
+
+///****** */
+// Stack (Primitive), Heap (Non-Primitive)
+
+let firstName = "Praful";
+let name = firstName;
+
+name = "Praful Bagde"
+// console.log(firstName);
+// console.log(name);
+
+let userOne = {
+    'name': 'Praful',
+    'email': 'prafulbagde@gmail.com'
+}
+
+let userTwo = userOne;
+userTwo.email = 'prafulbagde25@gmail.com';
+
+console.log(userOne.email);
+console.log(userTwo.email);
