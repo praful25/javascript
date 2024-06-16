95d84bfe7c71a49dd398b9923e5a77b9ce0e8c92
Praful Bagde <78199648+praful25@users.noreply.github.com>
2024-06-16 15:48:39 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+let firstName = "Praful";
+let lastName = "Bagde";
+let age = 30;
+
+// String concatenation
+// console.log(firstName + lastName + age);
+
+// String interpolation
+// console.log(`Hello my name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} and my age is ${age}`);
+
+// console.log(firstName.length);
+// console.log(firstName.length);
+
+let str = new String("Praful");
+// console.log(str.__proto__);
+// console.log(str.length);
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+
+// let newStr = "       Hello world     "
+// console.log(newStr.trim());
+
+const url = "https://prafulbagde/dev%praful";
+// console.log(url);
+// url.replace('exercise_js.asp', 'ok');
+// console.log(url);
+// console.log(url.replace('%praful', '-praful'));
+// console.log(url.includes('praful'));
+
+let nameStr = "praful-ankit-raj-pankaj";
+console.log(nameStr.split('-'));
+console.log(typeof nameStr);
