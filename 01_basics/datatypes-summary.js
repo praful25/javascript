b3c7353dba2781797bdff77546672f425c12c633
Praful Bagde <78199648+praful25@users.noreply.github.com>
2024-06-16 09:25:39 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+//  Primitive
+
+// 7 Types: Number, String, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non Primitive)
+
+// Array, Object, Functions
+
+const indianSuperHeros = ['shaktiman', 'naagraj', 'doga'];
+// console.log(typeof indianSuperHeros);
+
+const myDetails = {
+    'name': 'Praful',
+    'age': 30,
+    'designation': 'SD'
+}
+// console.log(typeof myDetails);
+
+const myFunc = function myFuntion() {
+    console.log("Hello World");
+}
+console.log(typeof myFunc);
