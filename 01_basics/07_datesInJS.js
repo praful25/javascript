0e77e3aa35210dae6a83fbda95fe4b44e488425f
Praful Bagde <78199648+praful25@users.noreply.github.com>
2024-06-22 06:32:47 +0000

    dates in JS

---
+++ 01_basics/07_datesInJS.js
@@ -0,0 +1,12 @@
+let myDate = new Date();
+// console.log(myDate); // in miliseconds Output: 2024-06-21T19:01:35.161Z
+// console.log(myDate.toString()); // Output: Fri Jun 21 2024 19:03:39 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // Output: Fri Jun 21 2024
+// console.log(myDate.toLocaleDateString()); // Ouput: 6/21/2024
+// console.log(myDate.toLocaleString()); // Output: 6/21/2024, 7:07:40 PM
+
+// let myCreateData = new Date(2024, 5, 22);
+// console.log(myCreateData.toDateString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
